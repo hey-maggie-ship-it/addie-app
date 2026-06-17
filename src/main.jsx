69fcd518126525190276d2d82009f29cf1585c1b
Maggie <maggielee0503@gmail.com>
2026-06-18 03:35:24 +0900

    Add Sentry error monitoring with browser tracing

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as Sentry from '@sentry/react'
 import posthog from 'posthog-js'
 import './index.css'
 import App from './App.jsx'
+
+Sentry.init({
+  dsn: 'https://c3fc4a4fdfae43e90f7ed2abfd25d0db@o4511582143774720.ingest.us.sentry.io/4511582200135680',
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 0.2,
+  environment: import.meta.env.MODE,
+})
 
 posthog.init('phc_pVWbzgzySDV7V5KiRHiT6nv9uzXWCXD3Vb8bXLbuvHxZ', {
   api_host: 'https://us.i.posthog.com',
